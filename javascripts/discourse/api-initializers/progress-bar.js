301f36beb73704f699389d2dad3f3d11240576b1
Coin-coin le Canapin <canapin@gmail.com>
2026-01-22 23:02:25 +0100

    Better body classes handling
    Progress bar body class is added when the component is rendered, and removed when the component is destroyed.

--- javascripts/discourse/api-initializers/progress-bar.js
+++ javascripts/discourse/api-initializers/progress-bar.js
@@ -2,13 +2,6 @@ import { apiInitializer } from "discourse/lib/api";
 import ProgressBar from "../components/progress-bar";
 
 export default apiInitializer((api) => {
-  if (!settings.outlet_name.length) {
-    return;
-  }
-
-  const body = document.querySelector("body");
-  if (settings.outlet_name !== "") {
-    body.classList.add(settings.outlet_name + "-progress-bar");
-  }
+  if (!settings.outlet_name) return;
   api.renderInOutlet(settings.outlet_name, ProgressBar);
 });
